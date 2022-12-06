b734a2f197f34259a75426016534451986a07779
Paula <paulaeliegeferreira@gmail.com>
2022-12-06 20:24:47 -0300

    projeto-final-pretalab

---
+++ src/pages/Portfolio.jsx
@@ -0,0 +1,10 @@
+import { Header } from '../components/Header'
+import portfolioimage from '../assets/portfolioo.svg'
+
+export function Portfolio() {
+    return(
+        <>
+        <Header text="Projetos" image={portfolioimage}/>
+        </>
+    )
+}
---
+++ src/pages/Sobre.jsx
@@ -0,0 +1,10 @@
+import { Header } from '../components/Header'
+import sobreimage from '../assets/sobre.svg'
+
+export function Sobre() {
+    return (
+        <>
+            <Header text="Minha Trajetória" image={sobreimage} />
+        </>
+    )
+}
